1721998f4e1a1b592d302d1c8bf55ef16a2c3285
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-25 13:16:21 -0700

    updated call backs with sub / mult / divide

--- callbacks.js
+++ callbacks.js
@@ -1,5 +1,3 @@
-const { cornsilk } = require("color-name")
-
 // function call back which is he 3rd *val in "adder"
 const adder = (a, b, cb) => {
     const sum = a + b
@@ -13,6 +11,35 @@ adder(5, 10, sum => {
 adder(1, 100, sum => {
     console.log(`The Sum is ${sum}`)
 })
+
+
+const subtract = (a, b, cb) => {
+    const sub = a - b
+    cb(sub)
+}
+
+subtract(55, 44, sub => {
+    console.log(`The subtracted sum is ${sub}`)
+})
+
+
+const multiply = (a, b, cb) => {
+    const mult = a * b
+    cb(mult)
+}
+ multiply(22, 9, mult => {
+    console.log(`The amount is ${mult}`)
+ })
+
+
+ const divide = (a, b, cb) => {
+     const divideMe = a / b
+     cb(divideMe)
+ }
+ divide(43853, 33, divideMe => {
+     console.log(`The amount is ${divideMe}`)
+ })
+
 
 
 // document.addEventListener('click', hotdog => {
